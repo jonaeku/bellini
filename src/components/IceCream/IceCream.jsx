dd706c739f9dc63a804662097d2dacd64115ecaf
Jona <jonaeku@gmx.de>
2022-02-13 00:27:43 +0100

    Fix: :iphone: work on responsive Design

--- src/components/IceCream/IceCream.jsx
+++ src/components/IceCream/IceCream.jsx
@@ -5,9 +5,10 @@ import useWindowDimensions from "../../useful/WindowSize";
 const IceCream = () => {
 
   return (
-    <div className="app__wrapper app__iceCream section__padding">
+    <div className="section__padding app__wrapper app__iceCream">
       <div>
         <h1 className="app__iceCream-h1">23 KÖSTLICHE EISSORTEN</h1>
+        <hr className="app__hr"/>
       </div>
       <div className="app__iceCream-img">
         <img className="app__iceCream-img" src={require("../../assets/eissorten.jpeg")} alt="header_img" />
--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,20 +1,13 @@
 import "./StartPage.css";
 
-import useWindowDimensions from "../../useful/WindowSize";
-
 const StartPage = () => {
-  const { height } = useWindowDimensions();
-
-  const componentHeight = height - 100;
-
   return (
     <div className="app__header app__wrapper section__padding">
       <div className="app__wrapper_info">
         <h2>Eiscafé-Snackbar-Lounge</h2>
         <hr className="app__header-hr"/>
         <h1 className="app__header-h1">
-          <p className="app__header-Welcome">Willkommen</p> 
-          im Eiscafé Bellini
+          <p className="app__header-Welcome"> <span>Willkommen</span><span className="app__header-h1" style={{color: "black"}}> im Eiscafé Bellini</span></p>
         </h1>
         <button type="button" className="custom__button">
           Über Uns
