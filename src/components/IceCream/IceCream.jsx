623217a5ee52b813522faeadff856a3549451457
Jona <jonaeku@gmx.de>
2022-02-12 23:08:16 +0100

    Feature: :sparkles: icecream component

--- src/components/IceCream/IceCream.jsx
+++ src/components/IceCream/IceCream.jsx
@@ -5,22 +5,13 @@ import useWindowDimensions from "../../useful/WindowSize";
 const IceCream = () => {
 
   return (
-    <div className="app__ice app__wrapper section__padding">
-      <div className="app__wrapper_info">
-        <h2>Eiscafé-Snackbar-Lounge</h2>
-        <hr className="app__header-hr"/>
-        <h1 className="app__header-h1">
-          <p className="app__header-Welcome">Willkommen</p> 
-          im Eiscafé Bellini
-        </h1>
-        {/* <button type="button" className="custom__button">
-          Über Uns
-        </button> */}
+    <div className="app__wrapper app__iceCream section__padding">
+      <div>
+        <h1 className="app__iceCream-h1">23 KÖSTLICHE EISSORTEN</h1>
       </div>
-
-      {/* <div className="app__wrapper_img">
-        <img src={require("../../assets/icecream.png")} alt="header_img" />
-      </div> */}
+      <div className="app__iceCream-img">
+        <img className="app__iceCream-img" src={require("../../assets/eissorten.jpeg")} alt="header_img" />
+      </div>
     </div>
   );
 };
