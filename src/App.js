2331fd06f74cc0af6890832ba27916d961091a41
Jona <jonaeku@gmx.de>
2022-02-13 21:09:51 +0100

    Feature: :sparkles: Opening Hours

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import IceCream from "./components/IceCream/IceCream";
 import Navbar from "./components/Navbar/Navbar";
+import OpeningHours from "./components/OpeningHours/OpeningHours";
 import StartPage from "./components/StartPage/StartPage";
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className="App">
       <Navbar />
       <StartPage />
+      <IceCream />
+      <OpeningHours />
       <IceCream />
     </div>
   );
---
+++ src/components/OpeningHours/OpeningHours.jsx
@@ -0,0 +1,23 @@
+import "./OpeningHours.css"
+
+const OpeningHours = () => {
+  return (
+    <div>
+      <div class="margin"></div>
+
+      <section class="parallax">
+        <div class="parallax-inner">
+          <div className="app__open-data">
+              <h1>ÖFFNUNGSZEITEN</h1> <br />
+              <h2>Montag - Sonntag</h2> <br />
+              <h2>9:30 - 20:00</h2>
+          </div>
+        </div>
+      </section>
+
+      <div class="margin"></div>
+    </div>
+  );
+};
+
+export default OpeningHours;
